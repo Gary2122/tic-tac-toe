4d8c4162ddfda1ee8ee7fb8f27c1ae18bd85ba06
Gary2122 <1580110606@qq.com>
2024-12-10 18:42:46 +0800

    修改配置

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'uno.css';
+// import store from './store'
 // import { ThemeProvider } from '@mui/material/styles';  // 导入 ThemeProvider
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-    <App />
+        <App />
   </StrictMode>
 )
