32cff957165b382646388dc17ff4306b518efc46
Gary2122 <1580110606@qq.com>
2024-12-11 18:00:00 +0800

    #REFACTOR: main.tsx增加import

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 import 'uno.css';
+
 // import store from './store'
 // import { ThemeProvider } from '@mui/material/styles';  // 导入 ThemeProvider
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <App />
-  </StrictMode>
-)
+    </StrictMode>
+);
